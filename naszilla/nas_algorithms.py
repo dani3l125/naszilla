e89e16d3a3c2cbce4d564037a0e23d9f60a62c63
Daniel yakovlev <daniel.ya.125@gmail.com>
2022-06-01 18:34:47 +0300

    k=800

--- naszilla/nas_algorithms.py
+++ naszilla/nas_algorithms.py
@@ -66,7 +66,7 @@ def run_nas_algorithm(algo_params, search_space, mp, k_alg):
     return result, val_result, data
 
 
-def knas(algo_params, search_space, mp, iterations=1, k=8):
+def knas(algo_params, search_space, mp, iterations=1, k=800):
     # run nas algorithm
     algo_params['total_queries'] = int(algo_params['total_queries'] / iterations)
     ps = copy.deepcopy(algo_params)
