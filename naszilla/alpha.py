2ada1f39c3a12b49cd8e560589461525e6e5d1c7
Daniel yakovlev <daniel.ya.125@gmail.com>
2022-10-02 17:14:14 +0300

    data stats

--- naszilla/alpha.py
+++ naszilla/alpha.py
@@ -4,8 +4,8 @@ import os
 from nas_201_api import NASBench201API as API
 import torch
 
-k_list = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13]
-coreset_iteration_sample_size_list = [20, 30, 40, 50]
+k_list = [6, 7, 8, 9, 10, 11, 12 ,13]
+coreset_iteration_sample_size_list = [20, 30, 40, 50, 60]
 median_sample_size_list = [20]
 
 d = 10
@@ -32,14 +32,17 @@ def coreset_stats(k, coreset_iteration_sample_size, median_sample_size):
         archs_val[1, i] = search_space.query_by_index(i).get_metrics('cifar100', 'x-valid')['loss']
         archs_val[2, i] = search_space.query_by_index(i).get_metrics('ImageNet16-120', 'x-valid')['loss']
 
-    cluster_best_vals = np.zeros(coreset_indexes.size)
-    cluster_representative_vals = np.zeros(coreset_indexes.size)
-    for label in range(coreset_indexes.size):
-        cluster_representative_vals[label] = archs_val[0, 1]
+    print(f'\n\nk = {k} | coreset iteration sample size = {coreset_iteration_sample_size} | median_sample_size = {median_sample_size} ## coreset size = {coreset_indexes.size} | cluster size:(mean, std) = ({mean}. {std})')
 
+    for i, dataset in enumerate(['cifar10', 'cifar100', 'ImageNet16-120']):
+        cluster_best_vals = np.ones(coreset_indexes.size) * 10000
+        cluster_representative_vals = np.zeros(coreset_indexes.size)
+        for arch_idx, label in enumerate(labels):
+            cluster_best_vals[label] = min(cluster_best_vals[label],  archs_val[i, arch_idx])
+        for label in range(coreset_indexes.size):
+            cluster_representative_vals[label] = archs_val[i, coreset_indexes[label]]
+            print(f'Dataset = {dataset} | Cluster = {label} | Cluster representative value = {cluster_representative_vals[label]} | Cluster best value = {cluster_best_vals[label]} | Difference = {np.abs(cluster_best_vals[label] - cluster_representative_vals[label])}')
 
-    print(
-        f'k = {k} | coreset iteration sample size = {coreset_iteration_sample_size} | median_sample_size = {median_sample_size} ## coreset size = {size} | cluster size:(mean, std) = ({mean}. {std})')
 
 def search_space_stats(num_of_optimums=30, knn=150):
     search_space =  API(os.path.expanduser('~/nas_benchmark_datasets/NAS-Bench-201-v1_0-e61699.pth'))
@@ -66,8 +69,7 @@ def search_space_stats(num_of_optimums=30, knn=150):
 
 
 if __name__ == '__main__':
-    search_space_stats()
     for k in k_list:
         for coreset_iteration_sample_size in coreset_iteration_sample_size_list:
             for median_sample_size in median_sample_size_list:
-                size, mean, std = coreset_stats(k, coreset_iteration_sample_size, median_sample_size)
+                coreset_stats(k, coreset_iteration_sample_size, median_sample_size)
