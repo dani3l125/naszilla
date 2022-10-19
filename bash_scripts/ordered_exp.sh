5748e13ec56aa21909a1865327859c242605aae8
Daniel yakovlev <daniel.ya.125@gmail.com>
2022-10-20 00:16:20 +0300

    q<=search space and experiments

--- bash_scripts/ordered_exp.sh
+++ bash_scripts/ordered_exp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in 1 2 3 4
+for i in 1 2 3 4 5 6 7 8 9 10 11 12
 do
   printf "\n\n\n####################\n####################\n Experiment $i\n####################\n####################\n\n\n"
   /home/daniel/anaconda3/envs/knas/bin/python /home/daniel/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \
