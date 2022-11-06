b1036f609e1f9dd5ae2c87647aa70805f8bf53f0
Daniel yakovlev <daniel.ya.125@gmail.com>
2022-11-06 20:30:16 +0200

    debug multithread

--- bash_scripts/dlc_ordered_exp.sh
+++ bash_scripts/dlc_ordered_exp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in 1 2 3 4 5
+for i in 1 2 3 4
 do
   screen -L -Logfile cifar10_exp$i -S cifar10_exp$i -dm srun --gpus=1 --cpus-per-task=6\
    --container-image=/users/feldman/dyakovlev/knas.sqsh --container-mounts=/users/feldman/dyakovlev/:/dyakovlev \
