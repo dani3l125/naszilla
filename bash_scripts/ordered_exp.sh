0143a853305a8371fd2504b357a3d6564c588458
Daniel yakovlev <daniel.ya.125@gmail.com>
2022-11-07 15:50:14 +0200

    debug multithread

--- bash_scripts/ordered_exp.sh
+++ bash_scripts/ordered_exp.sh
@@ -4,18 +4,20 @@ TRIALS=1
 
 for i in 6 7 8 9
 do
-  printf "\n\n\n####################\n####################\n Experiment $i\n####################\n####################\n\n\n"
-  /home/daniel/anaconda3/envs/knas/bin/python /home/daniel/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \
+  screen -L -Logfile cidar10_exp$i -S \
+    "/home/daniel/anaconda3/envs/knas/bin/python /home/daniel/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \
   --algo_params all_algos --queries 300 --trials $TRIALS --k_alg 1 --cfg /home/daniel/naszilla/naszilla/config_files/$i.yaml\
-    --dataset cifar10
+    --dataset cifar10"
 
-  /home/daniel/anaconda3/envs/knas/bin/python /home/daniel/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \
+  screen -L -Logfile cidar10_exp$i -S \
+    "/home/daniel/anaconda3/envs/knas/bin/python /home/daniel/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \
   --algo_params all_algos --queries 300 --trials $TRIALS --k_alg 1 --cfg /home/daniel/naszilla/naszilla/config_files/$i.yaml\
-    --dataset cifar100
+    --dataset cifar100"
 
-  /home/daniel/anaconda3/envs/knas/bin/python /home/daniel/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \
+  screen -L -Logfile cidar10_exp$i -S \
+    "/home/daniel/anaconda3/envs/knas/bin/python /home/daniel/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \
   --algo_params all_algos --queries 300 --trials $TRIALS --k_alg 1 --cfg /home/daniel/naszilla/naszilla/config_files/$i.yaml\
-    --dataset ImageNet16-120
+    --dataset ImageNet16-120"
 
 #  /home/daniel/anaconda3/envs/knas/bin/python /home/daniel/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \
 #  --algo_params random --queries 300 --trials 5 --k_alg 1 --cfg /home/daniel/naszilla/naszilla/config_files/$i.yaml\
