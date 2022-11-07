9d59fa36d6165e5a89bd3fe133f2925bff953916
Daniel yakovlev <daniel.ya.125@gmail.com>
2022-11-07 14:59:06 +0200

    debug multithread

--- bash_scripts/dlc_ordered_exp.sh
+++ bash_scripts/dlc_ordered_exp.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
-for i in 1 2
+
+TRIALS=1
+
+let NTHREADS=$TRIALS*4+1
+
+for i in 1 2 3 4 5
 do
-  screen -L -Logfile cifar10_local_exp$i -S cifar10_exp$i -dm srun --gpus=1 --cpus-per-task=6\
+  screen -L -Logfile cifar10_exp$i -S cifar10_exp$i -dm srun --gpus=1 --mincpus=$NTHREADS -w dgx01, dgx02, dgx03, dgx04\
    --container-image=/users/feldman/dyakovlev/knas.sqsh --container-mounts=/users/feldman/dyakovlev/:/dyakovlev \
    /bin/bash -c "export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/naszilla\";export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/nasbench\";
   cd /dyakovlev/naszilla; printf \"\n\n\n####################\n Experiment $i\n####################\n\n\n\"
   python /dyakovlev/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \\
-  --algo_params local_search --queries 300 --trials 1 --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\\
+  --algo_params all_algos --queries 300 --trials $TRIALS --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\\
     --dataset cifar10"
 
-  screen -L -Logfile cifar100_local_exp$i -S cifar100_exp$i -dm srun --gpus=1 --cpus-per-task=6\
+  screen -L -Logfile cifar100_exp$i -S cifar100_exp$i -dm srun --gpus=1 --mincpus=$NTHREADS -w dgx01, dgx02, dgx03, dgx04\
    --container-image=/users/feldman/dyakovlev/knas.sqsh --container-mounts=/users/feldman/dyakovlev/:/dyakovlev \
    /bin/bash -c "export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/naszilla\";export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/nasbench\";
   cd /dyakovlev/naszilla; printf \"\n\n\n####################\n Experiment $i\n####################\n\n\n\"
   python /dyakovlev/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \\
-  --algo_params local_search --queries 300 --trials 1 --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\\
+  --algo_params all_algos --queries 300 --trials $TRIALS --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\\
     --dataset cifar100"
 
-  screen -L -Logfile imagenet_local_exp$i -S imagenet_exp$i -dm srun --gpus=1 --cpus-per-task=6\
+  screen -L -Logfile imagenet_exp$i -S imagenet_exp$i -dm srun --gpus=1 --mincpus=$NTHREADS -w dgx01, dgx02, dgx03, dgx04\
    --container-image=/users/feldman/dyakovlev/knas.sqsh --container-mounts=/users/feldman/dyakovlev/:/dyakovlev \
    /bin/bash -c "export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/naszilla\";export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/nasbench\";
   cd /dyakovlev/naszilla; printf \"\n\n\n####################\n Experiment $i\n####################\n\n\n\"
   python /dyakovlev/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \\
-  --algo_params local_search --queries 300 --trials 1 --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\\
+  --algo_params all_algos --queries 300 --trials $TRIALS --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\\
     --dataset ImageNet16-120"
 
 #  python /dyakovlev/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \
