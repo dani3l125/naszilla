a516a1af6cd4c31e10b6be5854f26bb4827abb08
Daniel yakovlev <daniel.ya.125@gmail.com>
2022-11-17 11:01:27 +0200

    better plots

--- naszilla/run_experiments.py
+++ naszilla/run_experiments.py
@@ -174,13 +174,13 @@ def run_experiments(args, save_dir):
             sota_val_result = 100 - np.load(f'sota_results/{algo_name}_{args.dataset}_val.npy')
             result = 100 - algorithm_results[algo_name][0]
             val_result = 100 - algorithm_val_results[algo_name][0]
-            ax1.plot(np.arange(10, 301, 10)[5:], sota_result, 'o-', label=label_mapping[algo_name]+', SOTA')
+            ax1.plot(np.arange(10, 301, 10), sota_result, 'o-', label=label_mapping[algo_name]+', SOTA')
             ax1.plot(np.arange(10, 301, 10), result[10::10], '^-', label=label_mapping[algo_name] + ', ours')
-            ax1.errorbar(x=np.arange(1, 301, 1)[5:], y=result[5:], yerr=algorithm_results[algo_name][1][5:],
+            ax1.errorbar(x=np.arange(1, 301, 1)[10:], y=result[10:], yerr=algorithm_results[algo_name][1][10:],
                          fmt='-', errorevery=10)
-            ax2.plot(np.arange(10, 301, 10)[5:], sota_val_result[5:], 'o-', label=label_mapping[algo_name]+', SOTA')
+            ax2.plot(np.arange(10, 301, 10), sota_val_result[10:], 'o-', label=label_mapping[algo_name]+', SOTA')
             ax2.plot(np.arange(10, 301, 10), val_result[10::10], '^-', label=label_mapping[algo_name]+', ours')
-            ax2.errorbar(x=np.arange(1, 301, 1)[5:], y=val_result[5:], yerr=algorithm_val_results[algo_name][1][5:],
+            ax2.errorbar(x=np.arange(1, 301, 1)[10:], y=val_result[10:], yerr=algorithm_val_results[algo_name][1][10:],
                          fmt='-', errorevery=10)
             np.save(
                 'plots/src_data/{}_{}_{}_{}_val'.format(cfg['figName'], args.dataset, compression_method, algo_name),
