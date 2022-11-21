68670c9ea036ff005724a157098de50701df3c1e
Daniel yakovlev <daniel.ya.125@gmail.com>
2022-11-21 15:59:32 +0200

    experiments

---
+++ bash_scripts/dlc_plots.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+TRIALS=1
+
+let NTHREADS=$TRIALS*4+1
+for i in 1 2 3 5 6 7 8 10
+do
+#  screen -L -Logfile cifar10_exp$i -S cifar10_exp$i -dm srun --gpus=1 --mincpus=$NTHREADS\
+#   --container-image=/users/feldman/dyakovlev/knas.sqsh --container-mounts=/users/feldman/dyakovlev/:/dyakovlev \
+#   /bin/bash -c "export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/naszilla\";export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/nasbench\";
+#  cd /dyakovlev/naszilla; printf \"\n\n\n####################\n Experiment $i\n####################\n\n\n\"
+#  python /dyakovlev/naszilla/naszilla/plot.py --search_space nasbench_201 \\
+#  --algo_params all_algos --queries 300 --trials $TRIALS --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\\
+#    --dataset cifar10"
+#
+#  screen -L -Logfile cifar100_exp$i -S cifar100_exp$i -dm srun --gpus=1 --mincpus=$NTHREADS\
+#   --container-image=/users/feldman/dyakovlev/knas.sqsh --container-mounts=/users/feldman/dyakovlev/:/dyakovlev \
+#   /bin/bash -c "export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/naszilla\";export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/nasbench\";
+#  cd /dyakovlev/naszilla; printf \"\n\n\n####################\n Experiment $i\n####################\n\n\n\"
+#  python /dyakovlev/naszilla/naszilla/plot.py --search_space nasbench_201 \\
+#  --algo_params all_algos --queries 300 --trials $TRIALS --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\\
+#    --dataset cifar100"
+
+  screen -L -Logfile imagenet_exp$i -S imagenet_exp$i -dm srun --gpus=1 --mincpus=$NTHREADS\
+   --container-image=/users/feldman/dyakovlev/knas.sqsh --container-mounts=/users/feldman/dyakovlev/:/dyakovlev \
+   /bin/bash -c "export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/naszilla\";export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/nasbench\";
+  cd /dyakovlev/naszilla; printf \"\n\n\n####################\n Experiment $i\n####################\n\n\n\"
+  python /dyakovlev/naszilla/naszilla/plot.py --search_space nasbench_201 \\
+  --algo_params all_algos --queries 300 --trials $TRIALS --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\\
+    --dataset ImageNet16-120"
+
+#  python /dyakovlev/naszilla/naszilla/plot.py --search_space nasbench_201 \
+#  --algo_params random --queries 300 --trials 5 --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\
+#    --dataset cifar10
+#
+#  python /dyakovlev/naszilla/naszilla/plot.py --search_space nasbench_201 \
+#  --algo_params random --queries 300 --trials 5 --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\
+#    --dataset cifar100
+#
+#  python /dyakovlev/naszilla/naszilla/plot.py --search_space nasbench_201 \
+#  --algo_params random --queries 300 --trials 5 --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\
+#    --dataset ImageNet16-120
+#
+#  python /dyakovlev/naszilla/naszilla/plot.py --search_space nasbench_201 \
+#  --algo_params evolution --queries 300 --trials 5 --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\
+#    --dataset cifar10
+#
+#  python /dyakovlev/naszilla/naszilla/plot.py --search_space nasbench_201 \
+#  --algo_params evolution --queries 300 --trials 5 --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\
+#    --dataset cifar100
+#
+#  python /dyakovlev/naszilla/naszilla/plot.py --search_space nasbench_201 \
+#  --algo_params evolution --queries 300 --trials 5 --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\
+#    --dataset ImageNet16-120
+#
+#  python /dyakovlev/naszilla/naszilla/plot.py --search_space nasbench_201 \
+#  --algo_params local_search --queries 300 --trials 5 --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\
+#    --dataset cifar10
+#
+#  python /dyakovlev/naszilla/naszilla/plot.py --search_space nasbench_201 \
+#  --algo_params local_search --queries 300 --trials 5 --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\
+#    --dataset cifar100
+#
+#  python /dyakovlev/naszilla/naszilla/plot.py --search_space nasbench_201 \
+#  --algo_params local_search --queries 300 --trials 5 --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/$i.yaml\
+#    --dataset ImageNet16-120
+done
+
--- naszilla/plot.py
+++ naszilla/plot.py
@@ -1,67 +1,85 @@
 import argparse
-import time
-import logging
 import sys
 import os
 import pickle
 import numpy as np
 import copy
 import yaml
-import threading
-import multiprocessing
 from cycler import cycler
 import matplotlib.pyplot as plt
 
-from naszilla.params import *
-from naszilla.nas_benchmarks import Nasbench101, Nasbench201, Nasbench301, KNasbench201
-from naszilla.nas_algorithms import run_nas_algorithm
 
 label_mapping = {'bananas':'BANANAS', 'local_search':'Local search', 'evolution':'Evolutionary search', 'random': 'Random search'}
-name = 'm15_ciss1_k8_path_cifar100_k_means_coreset_orig_dist'
-dataset = 'cifar100'
-src_path = 'plots/src_data'
 
-if __name__ == '__main__':
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(9, 4.5), tight_layout=True)
-    custom_cycler = cycler(color=['r', 'r', 'r', 'g', 'g', 'g', 'b', 'b', 'b', 'y', 'y', 'y'])
-    ax1.set_xlabel('Queries')
-    ax1.set_ylabel('Best train accuracy')
-    ax2.set_xlabel('Queries')
-    ax2.set_ylabel('Best validation accuracy')
-    ax1.set_prop_cycle(custom_cycler)
-    ax2.set_prop_cycle(custom_cycler)
+def plot_experiments(args, save_dir):
+    # read configuration
+    cfg = yaml.safe_load(open(args.cfg, 'r')) if args.k_alg else None
+
+    for compression_method in ['k_means_coreset_orig_dist', 'k_means_coreset', 'uniform', 'k_medoids']:
+
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(9, 4.5), tight_layout=True)
+        custom_cycler = cycler(color=['r', 'r', 'r', 'g', 'g', 'g', 'b','b', 'b', 'y', 'y', 'y'])
+        ax1.set_xlabel('Queries')
+        ax1.set_ylabel('Best train accuracy')
+        ax2.set_xlabel('Queries')
+        ax2.set_ylabel('Best validation accuracy')
+        ax1.set_prop_cycle(custom_cycler)
+        ax2.set_prop_cycle(custom_cycler)
+        def inverse(x):
+            return x
+        def forward(x):
+            return x
+        ax1.set_yscale('function', functions=(forward, inverse))
+        ax2.set_yscale('function', functions=(forward, inverse))
+        if not os.path.exists('plots'):
+            os.makedirs('plots')
+        if not os.path.exists('plots/src_data'):
+            os.makedirs('plots/src_data')
+        for algo_name in label_mapping.keys():
+            sota_result = 100 - np.load(f'sota_results/{algo_name}_{args.dataset}.npy')
+            sota_val_result = 100 - np.load(f'sota_results/{algo_name}_{args.dataset}_val.npy')
+            if not os.path.exists('plots/src_data/{}_{}_{}_{}'.format(cfg['figName'], args.dataset, compression_method, algo_name)):
+                break
+            result = np.load(
+                'plots/src_data/{}_{}_{}_{}'.format(cfg['figName'], args.dataset, compression_method, algo_name))
+            val_result = np.load(
+                'plots/src_data/{}_{}_{}_{}_val'.format(cfg['figName'], args.dataset, compression_method, algo_name))
+            ax1.plot(np.arange(10, 301, 10), sota_result, '--', label=label_mapping[algo_name]+', SOTA')
+            ax1.plot(np.arange(10, 301, 50), result[9::10], '^-', label=label_mapping[algo_name] + ', ours')
+            ax1.plot(x=np.arange(1, 301, 1), y=result, fmt='-')
+            ax2.plot(np.arange(10, 301, 10), sota_val_result, '--', label=label_mapping[algo_name]+', SOTA')
+            ax2.plot(np.arange(10, 301, 50), val_result[9::10], '^-', label=label_mapping[algo_name]+', ours')
+            ax2.errorbar(x=np.arange(1, 301, 1), y=val_result, fmt='-')
+
+        ax1.legend()
+        ax2.legend()
+        plt.savefig('plots/{}_{}_{}.png'.format(cfg['figName'], args.dataset, compression_method))
 
 
-    def inverse(x):
-        return x
+def main(args):
+
+    plot_experiments(args)
 
 
-    def forward(x):
-        return x
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Args for BANANAS experiments')
+    parser.add_argument('--trials', type=int, default=500, help='Number of trials')
+    parser.add_argument('--queries', type=int, default=150,
+                        help='Max number of queries/evaluations each NAS algorithm gets')
+    parser.add_argument('--search_space', type=str, default='nasbench_101', help='nasbench_101, _201, or _301')
+    parser.add_argument('--dataset', type=str, default='cifar10', help='cifar10, 100, or imagenet (for nasbench201)')
+    parser.add_argument('--mf', type=bool, default=False, help='Multi fidelity: true or false (for nasbench101)')
+    parser.add_argument('--metann_params', type=str, default='standard', help='which parameters to use')
+    parser.add_argument('--algo_params', type=str, default='simple_algos', help='which parameters to use')
+    parser.add_argument('--output_filename', type=str, default='round', help='name of output files')
+    parser.add_argument('--save_dir', type=str, default='results_output', help='name of save directory')
+    parser.add_argument('--save_specs', type=bool, default=False, help='save the architecture specs')
+    parser.add_argument('--save_sota', type=int, default=0, help='save the convergence result to a numpy array')
+    parser.add_argument('--k_alg', type=int, default=0, help='use iterative k algorithm')
+    parser.add_argument('--sample_size_graphs', type=int, default=0, help='plot graphs with coreset size independent variable')
+    parser.add_argument('--k_graphs', type=int, default=0, help='plot graphs with coreset size independent variable')
+    parser.add_argument('--cfg', type=str, default='/home/daniel/naszilla/naszilla/knas_config.yaml',
+                        help='path to configuration file')
 
-    ax1.set_yscale('function', functions=(forward, inverse))
-    ax2.set_yscale('function', functions=(forward, inverse))
-    if not os.path.exists('plots'):
-        os.makedirs('plots')
-    if not os.path.exists('plots/src_data'):
-        os.makedirs('plots/src_data')
-    for algo_name in label_mapping.keys():
-        if not os.path.exists(os.path.join(src_path, name+'_'+algo_name+'.npy')):
-            break
-        result = np.load(os.path.join(src_path, name+'_'+algo_name+'.npy'))
-        val_result = np.load(os.path.join(src_path, name+'_'+algo_name+'_val.npy'))
-
-
-        sota_result = 100 - np.load(f'/home/daniel/naszilla/sota_results/{algo_name}_{dataset}.npy')
-        sota_val_result = 100 - np.load(f'/home/daniel/naszilla/sota_results/{algo_name}_{dataset}_val.npy')
-        ax1.plot(np.arange(10, 301, 10), sota_result, 'o-', label=label_mapping[algo_name] + ', SOTA')
-        ax1.plot(np.arange(10, 301, 10), result[9::10], '^-', label=label_mapping[algo_name] + ', ours')
-        ax1.plot(x=np.arange(1, 301, 1), y=result,
-                     fmt='-')
-        ax2.plot(np.arange(10, 301, 10), sota_val_result, 'o-', label=label_mapping[algo_name] + ', SOTA')
-        ax2.plot(np.arange(10, 301, 10), val_result[9::10], '^-', label=label_mapping[algo_name] + ', ours')
-        ax2.plot(x=np.arange(1, 301, 1), y=val_result,
-                     fmt='-')
-    ax1.legend()
-    ax2.legend()
-    plt.savefig('plots/{}.png'.format(name))
+    args = parser.parse_args()
+    main(args)
