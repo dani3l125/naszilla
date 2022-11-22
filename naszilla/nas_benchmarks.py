13a6884b295a186829a14d1163c27998f1cbfd79
Daniel yakovlev <daniel.ya.125@gmail.com>
2022-11-22 09:51:18 +0200

    check trials

--- naszilla/nas_benchmarks.py
+++ naszilla/nas_benchmarks.py
@@ -483,7 +483,7 @@ class KNasbench201(Nasbench201):
                  compression_method='k_medoids',
                  compression_args=None,
                  points_alg='evd',
-                 is_debug=True
+                 is_debug=False
                  ):
         super().__init__(dataset, data_folder, version, is_debug=is_debug)
         self._is_updated_distances = False
