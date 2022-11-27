ed1ee2ed6b1e177c7f505c59fd537f799cbfd60b
Daniel yakovlev <daniel.ya.125@gmail.com>
2022-11-27 16:47:49 +0200

    statistics updated

--- naszilla/alpha.py
+++ naszilla/alpha.py
@@ -1,5 +1,6 @@
 import numpy as np
 from naszilla.coresets.k_means_coreset_via_robust_median import k_means_coreset_via_robust_median
+from naszilla.nas_benchmarks import KNasbench201
 import os
 from nas_201_api import NASBench201API as API
 from naszilla.nas_bench_201.distances import *
@@ -7,27 +8,40 @@ from naszilla.nas_bench_201.cell_201 import Cell201
 import argparse
 import torch
 
+k_means_coreset_args = {'coreset_iteration_sample_size': 1,
+                        'k': 50,
+                        'k_ratio': 0,
+                        'median_sample_size': 40,
+                        'tau_for_the_sampled_set': None,
+                        'tau_for_the_original_set': None,
+                        'Replace_in_coreset_sample': 0,
+                        'use_threshold_method': 0,
+                        'random_generation': 0,
+                        'r': 2}
+
 parser = argparse.ArgumentParser(description='Args for BANANAS experiments')
 parser.add_argument('--dist', type=str, default='lev', help='Number of trials')
 args = parser.parse_args()
 
-k_list = [4, 6, 8, 10, 12 ,14]
+k_list = [4, 6, 8, 10, 12, 14]
 coreset_iteration_sample_size_list = [1]
 median_sample_size_list = [20]
 
+DEFAULT_K = 12
+DEFAULT_CISS = 1
+DEFAULT_SAMPLE_SIZE_LIST = 20
 
-search_space =  API(os.path.expanduser('~/nas_benchmark_datasets/NAS-Bench-201-v1_0-e61699.pth'))
-# search_space = torch.load('/home/daniel/nas_benchmark_datasets/NAS-Bench-mini.pth')
+# search_space =  API(os.path.expanduser('~/nas_benchmark_datasets/NAS-Bench-201-v1_0-e61699.pth'))
+search_space = torch.load('/home/daniel/nas_benchmark_datasets/NAS-Bench-mini.pth')
 archs_val = np.zeros((3, len(search_space)))
 for i in range(len(search_space)):
     archs_val[0, i] = search_space.query_by_index(i).get_metrics('cifar10-valid', 'x-valid')['accuracy']
     archs_val[1, i] = search_space.query_by_index(i).get_metrics('cifar100', 'x-valid')['accuracy']
     archs_val[2, i] = search_space.query_by_index(i).get_metrics('ImageNet16-120', 'x-valid')['accuracy']
 
-
 d = 10
 dataset = 'cifar100'
-dist_matrix = np.load(f'/home/daniel/distance_matrices/{args.dist}_dist.npy')
+dist_matrix = np.load(f'/home/daniel/distances/{args.dist}_dist.npy')
 P = np.zeros_like(dist_matrix)[:, :d]
 
 distace_functions = {
@@ -35,12 +49,13 @@ distace_functions = {
     'path': path_distance,
     'lev': lev_distance,
     'nasbot': nasbot_distance,
-#    'real': real_distance
+    #    'real': real_distance
 }
 
+
 def calculate_distance_mat(dist_name):
-    if os.path.isfile(f'/home/daniel/distance_matrices/{dist_name}_dist.npy'):
-        return
+    if os.path.isfile(f'/home/daniel/distances/{dist_name}_dist.npy'):
+        return np.load(f'/home/daniel/distances/{dist_name}_dist.npy')
     dist_matrix = np.zeros((len(search_space), len(search_space)))
     for i, str1 in enumerate(search_space.meta_archs):
         for j, str2 in enumerate(search_space.meta_archs):
@@ -49,14 +64,14 @@ def calculate_distance_mat(dist_name):
             else:
                 dist_matrix[i, j] = distace_functions[dist_name](Cell201(str1), Cell201(str2))
 
-    np.save(f'/home/daniel/distance_matrices/{dist_name}_dist.npy', dist_matrix)
+    np.save(f'/home/daniel/distances/{dist_name}_dist.npy', dist_matrix)
+    return dist_matrix
 
 
 def coreset_stats(k, coreset_iteration_sample_size, median_sample_size, num_of_optimums=30):
     _, _, coreset, _, coreset_indexes = k_means_coreset_via_robust_median(P, dist_matrix, k=k,
                                                                           coreset_iteration_sample_size=coreset_iteration_sample_size,
                                                                           median_sample_size=median_sample_size)
-
 
     points2coreset_dist_mat = dist_matrix[:, coreset_indexes]
     labels = np.argmin(points2coreset_dist_mat, axis=1)
@@ -65,7 +80,8 @@ def coreset_stats(k, coreset_iteration_sample_size, median_sample_size, num_of_o
     mean = np.mean(sizes)
     std = np.std(sizes)
 
-    print(f'\nk = {k} | coreset iteration sample size = {coreset_iteration_sample_size} | median sample size = {median_sample_size}')
+    print(
+        f'\nk = {k} | coreset iteration sample size = {coreset_iteration_sample_size} | median sample size = {median_sample_size}')
     print(f'coreset size = {coreset_indexes.size}')
     print(f'cluster size:(mean, std) = ({mean}. {std})')
 
@@ -80,19 +96,22 @@ def coreset_stats(k, coreset_iteration_sample_size, median_sample_size, num_of_o
         #     optimum_accuracy_dists[j] = np.abs(archs_val[i, optimum_indexes[j]] - archs_val[i, representatives_indexes[j]])
         optimum_accuracy_dists = np.abs(archs_val[i, optimum_indexes] - archs_val[i, representatives_indexes])
         optimum_euclidean_dists = dist_matrix[optimum_indexes, representatives_indexes]
-        print(f'\t{num_of_optimums} best architectures validation accuracy distance from representative: {optimum_accuracy_dists}')
+        print(
+            f'\t{num_of_optimums} best architectures validation accuracy distance from representative: {optimum_accuracy_dists}')
         print(
             f'\t{num_of_optimums} best architectures {args.dist} (pseudo euclidean) distance from representative: {optimum_euclidean_dists}')
         cluster_accuracy_distance = np.zeros(optimum_labels.size)
         cluster_euclidean_distance = np.zeros(optimum_labels.size)
         for idx, label in enumerate(optimum_labels):
             label_indexes = np.where(labels == label)
-            cluster_accuracy_distance[idx] = np.mean(np.abs(archs_val[i, label_indexes] - archs_val[i, representatives_indexes[idx]]))
+            cluster_accuracy_distance[idx] = np.mean(
+                np.abs(archs_val[i, label_indexes] - archs_val[i, representatives_indexes[idx]]))
             cluster_euclidean_distance[idx] = np.mean(dist_matrix[representatives_indexes[idx], label_indexes])
         print(f'\t{num_of_optimums} best architecture\'s clusters:')
         print(f'\tcluster sizes: {sizes[optimum_labels]}')
         print(f'\tAverage accuracy distance from representative (in cluster): {cluster_accuracy_distance}')
-        print(f'\tAverage {args.dist} (euclidean) distance from representative (in cluster): {cluster_euclidean_distance}')
+        print(
+            f'\tAverage {args.dist} (euclidean) distance from representative (in cluster): {cluster_euclidean_distance}')
 
         #
         # optimum_indexes = np.flip(np.argsort(archs_val[i]))
@@ -121,9 +140,8 @@ def coreset_stats(k, coreset_iteration_sample_size, median_sample_size, num_of_o
 
 
 def search_space_stats(num_of_optimums=30, knn=150):
-
     for i, dataset in enumerate(['cifar10', 'cifar100', 'ImageNet16-120']):
-        optimum_indexes = np.argsort(archs_val[i])[:num_of_optimums]
+        optimum_indexes = np.argsort(archs_val[i])[::-1][:num_of_optimums]
         knn_arr = np.argsort(dist_matrix[optimum_indexes])[:, :knn]
         for j, optimum in enumerate(optimum_indexes):
             knn_vals = []
@@ -132,14 +150,59 @@ def search_space_stats(num_of_optimums=30, knn=150):
             print(f'{dataset} Dataset | {j} optimum | {knn_vals}')
 
 
+def cluster_accuracy_statistics(space, dist_matrix):
+    for i, dataset in enumerate(['cifar10', 'cifar100', 'ImageNet16-120']):
+        clusters_num = space.labels.max() + 1
+        clusters_best_values = -1 * np.ones((clusters_num,))
+        clusters_maximal_distances_acc = -1 * np.ones((clusters_num,))
+        clusters_center_distances_acc = -1 * np.ones((clusters_num,))
+        clusters_maximal_distances_metric = -1 * np.ones((clusters_num,))
+        clusters_center_distances_metric = -1 * np.ones((clusters_num,))
+        evaluated_indexes_array = np.array(space.old_nasbench.evaluated_indexes)
+        for label in range(clusters_num):
+            label_values = archs_val[i][evaluated_indexes_array
+            [np.argwhere(space.labels == label)].T[0]]
+            clusters_best_values[label] = label_values.max()
+            clusters_maximal_distances_acc[label] = clusters_best_values[label] - label_values.min()
+            clusters_center_distances_acc[label] = clusters_best_values[label] - archs_val[i][
+                space.coreset_indexes[label]]
+            clusters_maximal_distances_metric[label] = dist_matrix[evaluated_indexes_array
+            [np.argwhere(space.labels == label)].T[0]][:, evaluated_indexes_array
+                                                     [np.argwhere(space.labels == label)].T[0]].max()
+            clusters_center_distances_metric[label] = dist_matrix[evaluated_indexes_array
+            [np.argwhere(space.labels == label)].T[0]][:, space.coreset_indexes[label]].max()
+
+        return {'clusters maximal accuracy distance (mean, std)': (
+                np.mean(clusters_maximal_distances_acc), np.std(clusters_maximal_distances_acc)),
+                'clusters maximal accuracy distance from center (mean, std)': (
+                np.mean(clusters_center_distances_acc), np.std(clusters_center_distances_acc)),
+                'clusters maximal metric distance (mean, std)': (
+                np.mean(clusters_maximal_distances_metric), np.std(clusters_maximal_distances_metric)),
+                'clusters maximal metric distance from center (mean, std)': (
+                np.mean(clusters_center_distances_metric), np.std(clusters_center_distances_metric))
+                }
+
+
+
+
 if __name__ == '__main__':
+    statistics_dict = {}
     for dist_name in distace_functions.keys():
         print(f'Calculating {dist_name} distance...')
-        calculate_distance_mat(dist_name)
+        dist_matrix = calculate_distance_mat(dist_name)
         print('Done!')
+        statistics_dict[dist_name] = {}
+        for compression_method in ['k_means_coreset_orig_dist', 'k_means_coreset', 'uniform', 'k_medoids']:
+            is_debug = False
+            space = KNasbench201(dataset=dataset, dist_type=dist_name, n_threads=4,
+                                 compression_method=compression_method,
+                                 compression_args=k_means_coreset_args,
+                                 points_alg='evd', is_debug=is_debug)
+            if is_debug:
+                dist_matrix = dist_matrix[:150][:,:150]
+            space.prune(0, 400)
+            statistics_dict[dist_name][compression_method] = cluster_accuracy_statistics(space, dist_matrix)
 
-    for k in k_list:
-        for coreset_iteration_sample_size in coreset_iteration_sample_size_list:
-            for median_sample_size in median_sample_size_list:
-                coreset_stats(k, coreset_iteration_sample_size, median_sample_size)
-
+    print(statistics_dict)
+    torch.save(statistics_dict, 'statistics.pth')
+    print('Statistics saved')
