b2a16f16f41d41cbddb0e13845304183024a28d1
Daniel yakovlev <daniel.ya.125@gmail.com>
2023-02-06 14:02:18 +0200

    experiments

--- bash_scripts/plots.sh
+++ bash_scripts/plots.sh
@@ -3,7 +3,7 @@
 TRIALS=1
 
 let NTHREADS=$TRIALS*4+1
-for i in 1 2 3 4
+for i in 1 2 3 4 5
 do
 #  /home/daniel/miniconda3/envs/knas/bin/python /home/daniel/naszilla/naszilla/plot.py --search_space nasbench_201 \
 #  --algo_params all_algos --queries 300 --trials $TRIALS --k_alg 1 --cfg /home/daniel/naszilla/naszilla/config_files/$i.yaml\
