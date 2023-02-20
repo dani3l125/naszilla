353e5dc475aa46ada6c8a917d3b5a0e8ec3e9c3b
Daniel yakovlev <daniel.ya.125@gmail.com>
2023-02-20 12:16:15 +0200

    debug q

--- bash_scripts/ordered_exp.sh
+++ bash_scripts/ordered_exp.sh
@@ -2,17 +2,17 @@
 
 TRIALS=1
 
-for i in 6 7 8 9
+for i in 1
 do
-  screen -L -Logfile cidar10_exp$i -S cidar10_exp$i -d -m bash -c\
-    "/home/daniel/anaconda3/envs/knas/bin/python /home/daniel/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \
-  --algo_params all_algos --queries 300 --trials $TRIALS --k_alg 1 --cfg /home/daniel/naszilla/naszilla/config_files/$i.yaml\
-    --dataset cifar10"
-
-  screen -L -Logfile cidar100_exp$i -S cidar100_exp$i -d -m bash -c\
-    "/home/daniel/anaconda3/envs/knas/bin/python /home/daniel/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \
-  --algo_params all_algos --queries 300 --trials $TRIALS --k_alg 1 --cfg /home/daniel/naszilla/naszilla/config_files/$i.yaml\
-    --dataset cifar100"
+#  screen -L -Logfile cidar10_exp$i -S cidar10_exp$i -d -m bash -c\
+#    "/home/daniel/anaconda3/envs/knas/bin/python /home/daniel/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \
+#  --algo_params all_algos --queries 300 --trials $TRIALS --k_alg 1 --cfg /home/daniel/naszilla/naszilla/config_files/$i.yaml\
+#    --dataset cifar10"
+#
+#  screen -L -Logfile cidar100_exp$i -S cidar100_exp$i -d -m bash -c\
+#    "/home/daniel/anaconda3/envs/knas/bin/python /home/daniel/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \
+#  --algo_params all_algos --queries 300 --trials $TRIALS --k_alg 1 --cfg /home/daniel/naszilla/naszilla/config_files/$i.yaml\
+#    --dataset cifar100"
 
   screen -L -Logfile imagenet_exp$i -S imagenet_exp$i -d -m bash -c\
     "/home/daniel/anaconda3/envs/knas/bin/python /home/daniel/naszilla/naszilla/run_experiments.py --search_space nasbench_201 \
