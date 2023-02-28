d738bd77ef73aa047968c0ae9b4b17fd45b82999
Daniel yakovlev <daniel.ya.125@gmail.com>
2023-02-28 17:53:35 +0200

    sbatch

--- bash_scripts/batch_experiments.sh
+++ bash_scripts/batch_experiments.sh
@@ -11,8 +11,8 @@ for dataset in ${datasets[@]}; do
   for cfg in ${cfgs[@]}; do
     for study in ${studies[@]}; do
       for algo in ${algos[@]}; do
-        sbatch -J "${algo}_${dataset}_cfg${cfg}_study${study}" --export=NTHREADS${NTHREADS},TRIALS${TRIALS},ALG=${algo},DATA=${dataset},STUDY=${study},CFG=${cfg}
-      sbatch -J "bananas_${dataset}_cfg${cfg}_study${study}" --export=NTHREADS${NTHREADS},TRIALS${TRIALS},ALG='bananas',DATA=${dataset},STUDY=${study},CFG=${cfg}
+        sbatch -J "${algo}_${dataset}_cfg${cfg}_study${study}" --export=NTHREADS${NTHREADS},TRIALS${TRIALS},ALG=${algo},DATA=${dataset},STUDY=${study},CFG=${cfg} sbatch one_experiment.sh
+      sbatch -J "bananas_${dataset}_cfg${cfg}_study${study}" --export=NTHREADS${NTHREADS},TRIALS${TRIALS},ALG='bananas',DATA=${dataset},STUDY=${study},CFG=${cfg} sbatch one_experiment_bananas.sh
       done
     done
   done
