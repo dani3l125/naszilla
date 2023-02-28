99e6a57bff2a28ae448553403063057c6e809915
Daniel yakovlev <daniel.ya.125@gmail.com>
2023-02-28 18:03:46 +0200

    sbatch

--- bash_scripts/batch_experiments.sh
+++ bash_scripts/batch_experiments.sh
@@ -11,8 +11,8 @@ for dataset in ${datasets[@]}; do
   for cfg in ${cfgs[@]}; do
     for study in ${studies[@]}; do
       for algo in ${algos[@]}; do
-        sbatch -J "${algo}_${dataset}_cfg${cfg}_study${study}" --export=NTHREADS${NTHREADS},TRIALS${TRIALS},ALG=${algo},DATA=${dataset},STUDY=${study},CFG=${cfg} one_experiment.sh
-      sbatch -J "bananas_${dataset}_cfg${cfg}_study${study}" --export=NTHREADS${NTHREADS},TRIALS${TRIALS},ALG='bananas',DATA=${dataset},STUDY=${study},CFG=${cfg} one_experiment_bananas.sh
+        sbatch -J "${algo}_${dataset}_cfg${cfg}_study${study}" --export=NTHREADS${NTHREADS},TRIALS${TRIALS},ALG=${algo},DATA=${dataset},STUDY=${study},CFG=${cfg} /dyakovlev/naszilla/naszilla/bash_scripts/one_experiment.sh
+      sbatch -J "bananas_${dataset}_cfg${cfg}_study${study}" --export=NTHREADS${NTHREADS},TRIALS${TRIALS},ALG='bananas',DATA=${dataset},STUDY=${study},CFG=${cfg} /dyakovlev/naszilla/naszilla/bash_scripts/one_experiment_bananas.sh
       done
     done
   done
