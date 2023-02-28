46144032fe47fb336259a7adcc17b1b64018c491
Daniel yakovlev <daniel.ya.125@gmail.com>
2023-03-01 00:52:39 +0200

    cifar100 random

--- bash_scripts/batch_experiments.sh
+++ bash_scripts/batch_experiments.sh
@@ -3,8 +3,8 @@
 NTHREADS=11
 TRIALS=10
 #datasets=('cifar10' 'cifar100' 'ImageNet16-120')
-datasets=('cifar100' 'ImageNet16-120')
-algos=('random' 'local_search' 'evolution')
+datasets=('cifar100')
+algos=('random')
 studies=(1)
 cfgs=('/dyakovlev/naszilla/naszilla/config_files/1.yaml')
 
@@ -13,7 +13,7 @@ for dataset in ${datasets[@]}; do
     for study in ${studies[@]}; do
       for algo in ${algos[@]}; do
         sbatch -J "${algo}_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG=${algo},DATA=${dataset},STUDY=${study},CFG=${cfg} bash_scripts/one_experiment.sh
-      sbatch -J "bananas_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${dataset},STUDY=${study},CFG=${cfg} bash_scripts/one_experiment_bananas.sh
+      #sbatch -J "bananas_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${dataset},STUDY=${study},CFG=${cfg} bash_scripts/one_experiment_bananas.sh
       done
     done
   done
