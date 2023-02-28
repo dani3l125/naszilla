521d4b9c0fae3a381f4b7c1917baf8c1da83bd31
Daniel yakovlev <daniel.ya.125@gmail.com>
2023-02-28 12:03:47 +0200

    debug

--- bash_scripts/dlc_ordered_study.sh
+++ bash_scripts/dlc_ordered_study.sh
@@ -68,7 +68,7 @@ let NTHREADS=6
 #--algo_params local_search --queries 300 --trials $TRIALS --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/1.yaml\\
 #  --dataset cifar100 --study 1"
 
-screen -L -Logfile imagenet_local_study -S imagenet_local_study -dm srun --gpus=0 --mincpus=$NTHREADS -w dgx01,dgx02,dgx03,dgx04,dgx05\
+screen -L -Logfile imagenet_local_study -S imagenet_local_study -dm srun --gpus=0 --mincpus=$NTHREADS -w dgx01,dgx02,dgx03,dgx04\
  --container-image=/users/feldman/dyakovlev/knas.sqsh --container-mounts=/users/feldman/dyakovlev/:/dyakovlev \
  /bin/bash -c "export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/naszilla\";export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/nasbench\";
 cd /dyakovlev/naszilla; printf \"\n\n\n####################\n Experiment 1 study\n####################\n\n\n\"
@@ -76,7 +76,7 @@ python /dyakovlev/naszilla/naszilla/run_experiments.py --search_space nasbench_2
 --algo_params local_search --queries 300 --trials $TRIALS --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/1.yaml\\
   --dataset ImageNet16-120 --study 1"
 
-screen -L -Logfile cifar10_bananas_study -S cifar10_bananas_study -dm srun --gpus=1 --mincpus=$NTHREADS -w dgx01,dgx02,dgx03,dgx04,dgx05\
+screen -L -Logfile cifar10_bananas_study -S cifar10_bananas_study -dm srun --gpus=1 --mincpus=$NTHREADS -w dgx01,dgx02,dgx03,dgx04\
  --container-image=/users/feldman/dyakovlev/knas.sqsh --container-mounts=/users/feldman/dyakovlev/:/dyakovlev \
  /bin/bash -c "export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/naszilla\";export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/nasbench\";
 cd /dyakovlev/naszilla; printf \"\n\n\n####################\n Experiment 1 study\n####################\n\n\n\"
@@ -84,7 +84,7 @@ python /dyakovlev/naszilla/naszilla/run_experiments.py --search_space nasbench_2
 --algo_params bananas --queries 300 --trials $TRIALS --k_alg 1 --cfg /dyakovlev/naszilla/naszilla/config_files/1.yaml\\
   --dataset cifar10 --study 1"
 #
-screen -L -Logfile cifar100_bananas_study -S cifar100_bananas_study -dm srun --gpus=1 --mincpus=$NTHREADS -w dgx01,dgx02,dgx03,dgx04,dgx05\
+screen -L -Logfile cifar100_bananas_study -S cifar100_bananas_study -dm srun --gpus=1 --mincpus=$NTHREADS -w dgx01,dgx02,dgx03,dgx04\
  --container-image=/users/feldman/dyakovlev/knas.sqsh --container-mounts=/users/feldman/dyakovlev/:/dyakovlev \
  /bin/bash -c "export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/naszilla\";export PYTHONPATH=\"${PYTHONPATH}:/dyakovlev/nasbench\";
 cd /dyakovlev/naszilla; printf \"\n\n\n####################\n Experiment 1 study\n####################\n\n\n\"
