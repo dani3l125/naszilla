27ef40e069e0bd1baed6404a4075b0621ce4fd22
Daniel yakovlev <daniel.ya.125@gmail.com>
2023-03-01 12:37:56 +0200

    one trial experiments

--- bash_scripts/batch_experiments.sh
+++ bash_scripts/batch_experiments.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 NTHREADS=11
-TRIALS=10
+TRIALS=1
 #datasets=('cifar10' 'cifar100' 'ImageNet16-120')
 #algos=('evolution' 'random' 'local_search')
 datasets=('cifar10' 'cifar100' 'ImageNet16-120')
 algos=('evolution' 'random' 'local_search')
 studies=(1)
 cfgs=('/dyakovlev/naszilla/naszilla/config_files/1.yaml')
+exps=(1 2)
 
 for dataset in ${datasets[@]}; do
-  for cfg in ${cfgs[@]}; do
+  for exp in ${exps[@]}; do
     for study in ${studies[@]}; do
       for algo in ${algos[@]}; do
-        sbatch -w dgx04 -J "singlethread_${algo}_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG=${algo},DATA=${dataset},STUDY=${study},CFG=${cfg} bash_scripts/one_experiment.sh
+        sbatch -w dgx05 -J "exp${exp}_${algo}_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG=${algo},DATA=${dataset},STUDY=${study},CFG=${cfg} bash_scripts/one_experiment.sh
       done
-      sbatch  -w dgx04 -J "singlehread_bananas_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${dataset},STUDY=${study},CFG=${cfg} bash_scripts/one_experiment_bananas.sh
+      sbatch  -w dgx05 -J "exp${exp}_bananas_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${dataset},STUDY=${study},CFG="/dyakovlev/naszilla/naszilla/config_files/${exp}.yaml" bash_scripts/one_experiment_bananas.sh
     done
   done
 done
