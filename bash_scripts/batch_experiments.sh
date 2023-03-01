20694cdd9866b5016dd081f879678b3d20e547ec
Daniel yakovlev <daniel.ya.125@gmail.com>
2023-03-01 09:45:11 +0200

    single threaded

--- bash_scripts/batch_experiments.sh
+++ bash_scripts/batch_experiments.sh
@@ -12,9 +12,9 @@ for dataset in ${datasets[@]}; do
   for cfg in ${cfgs[@]}; do
     for study in ${studies[@]}; do
       for algo in ${algos[@]}; do
-        sbatch -w dgx04 -J "${algo}_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG=${algo},DATA=${dataset},STUDY=${study},CFG=${cfg} bash_scripts/one_experiment.sh
+        sbatch -J "${algo}_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG=${algo},DATA=${dataset},STUDY=${study},CFG=${cfg} bash_scripts/one_experiment.sh
       done
-      sbatch -w dgx04 -J "bananas_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${dataset},STUDY=${study},CFG=${cfg} bash_scripts/one_experiment_bananas.sh
+      sbatch -J "bananas_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${dataset},STUDY=${study},CFG=${cfg} bash_scripts/one_experiment_bananas.sh
     done
   done
 done
