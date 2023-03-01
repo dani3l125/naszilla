a0b755f3b7229ccd5e78e9c96acaceb7b888c3ca
Daniel yakovlev <daniel.ya.125@gmail.com>
2023-03-01 14:54:31 +0200

    one trial experiments

--- bash_scripts/batch_experiments.sh
+++ bash_scripts/batch_experiments.sh
@@ -15,9 +15,9 @@ for index in 1 2 3; do
   for exp in ${exps[@]}; do
     for study in ${studies[@]}; do
       for algo in ${algos[@]}; do
-        sbatch -w dgxs[index] -J "exp${exp}_${algo}_${datasets[index]}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG=${algo},DATA=${datasets[index]},STUDY=${study},CFG="/dyakovlev/naszilla/naszilla/config_files/${exp}.yaml" bash_scripts/one_experiment.sh
+        sbatch -w dgxs[${index}] -J "exp${exp}_${algo}_${datasets[${index}]}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG=${algo},DATA=${datasets[${index}]},STUDY=${study},CFG="/dyakovlev/naszilla/naszilla/config_files/${exp}.yaml" bash_scripts/one_experiment.sh
       done
-      sbatch  -w dgx05 -J "exp${exp}_bananas_${datasets[index]}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${datasets[index]},STUDY=${study},CFG="/dyakovlev/naszilla/naszilla/config_files/${exp}.yaml" bash_scripts/one_experiment_bananas.sh
+      sbatch  -w dgx05 -J "exp${exp}_bananas_${datasets[${index}]}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${datasets[${index}]},STUDY=${study},CFG="/dyakovlev/naszilla/naszilla/config_files/${exp}.yaml" bash_scripts/one_experiment_bananas.sh
     done
   done
 done
