143c0ae0c608554dfa4d9c9b2a8df7d326f0a009
Daniel yakovlev <daniel.ya.125@gmail.com>
2023-03-01 12:24:57 +0200

    multithreaded

--- bash_scripts/batch_experiments.sh
+++ bash_scripts/batch_experiments.sh
@@ -4,8 +4,8 @@ NTHREADS=11
 TRIALS=10
 #datasets=('cifar10' 'cifar100' 'ImageNet16-120')
 #algos=('evolution' 'random' 'local_search')
-datasets=('ImageNet16-120')
-algos=('evolution')
+datasets=('cifar10' 'cifar100' 'ImageNet16-120')
+algos=('evolution' 'random' 'local_search')
 studies=(1)
 cfgs=('/dyakovlev/naszilla/naszilla/config_files/1.yaml')
 
@@ -13,9 +13,9 @@ for dataset in ${datasets[@]}; do
   for cfg in ${cfgs[@]}; do
     for study in ${studies[@]}; do
       for algo in ${algos[@]}; do
-        sbatch -w dgx02 -J "multithread_${algo}_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG=${algo},DATA=${dataset},STUDY=${study},CFG=${cfg} bash_scripts/one_experiment.sh
+        sbatch -w dgx03 -J "multithread_${algo}_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG=${algo},DATA=${dataset},STUDY=${study},CFG=${cfg} bash_scripts/one_experiment.sh
       done
-      sbatch  -w dgx02 -J "multithread_bananas_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${dataset},STUDY=${study},CFG=${cfg} bash_scripts/one_experiment_bananas.sh
+      sbatch  -w dgx03 -J "multithread_bananas_${dataset}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${dataset},STUDY=${study},CFG=${cfg} bash_scripts/one_experiment_bananas.sh
     done
   done
 done
