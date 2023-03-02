e8cef0781c8dd3984d8f2ac848ae9bc1f837d416
Daniel yakovlev <daniel.ya.125@gmail.com>
2023-03-02 10:01:07 +0200

    bananas experiment

--- bash_scripts/batch_experiments.sh
+++ bash_scripts/batch_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TRIALS=10
-NTHREADS=11
+TRIALS=1
+NTHREADS=2
 #datasets=('cifar10' 'cifar100' 'ImageNet16-120')
 #algos=('evolution' 'random' 'local_search')
 datasets=('cifar10' 'cifar100' 'ImageNet16-120')
@@ -13,10 +13,10 @@ exps=(3)
 for index in 0 1 2; do
   for exp in ${exps[@]}; do
     for study in ${studies[@]}; do
-      for algo in ${algos[@]}; do
-        sbatch -c ${NTHREADS} -J "final_exp${exp}_${algo}_${datasets[${index}]}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG=${algo},DATA=${datasets[${index}]},STUDY=${study},CFG="/dyakovlev/naszilla/naszilla/config_files/${exp}.yaml" bash_scripts/one_experiment.sh
-      done
-      #sbatch -c ${NTHREADS} -J "${exp}_bananas_${datasets[${index}]}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${datasets[${index}]},STUDY=${study},CFG="/dyakovlev/naszilla/naszilla/config_files/${exp}.yaml" bash_scripts/one_experiment_bananas.sh
+#      for algo in ${algos[@]}; do
+#        sbatch -c ${NTHREADS} -J "final_exp${exp}_${algo}_${datasets[${index}]}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG=${algo},DATA=${datasets[${index}]},STUDY=${study},CFG="/dyakovlev/naszilla/naszilla/config_files/${exp}.yaml" bash_scripts/one_experiment.sh
+#      done
+      sbatch -c ${NTHREADS} -J "${exp}_bananas_${datasets[${index}]}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${datasets[${index}]},STUDY=${study},CFG="/dyakovlev/naszilla/naszilla/config_files/${exp}.yaml" bash_scripts/one_experiment_bananas.sh
     done
   done
 done
