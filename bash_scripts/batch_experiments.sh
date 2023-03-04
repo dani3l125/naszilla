25d1f12a7b2ba923bb2ff8e933c27b1086f84df0
Daniel yakovlev <daniel.ya.125@gmail.com>
2023-03-04 15:36:55 +0200

    real distance experiments

--- bash_scripts/batch_experiments.sh
+++ bash_scripts/batch_experiments.sh
@@ -6,7 +6,7 @@ datasets=('cifar10' 'cifar100' 'ImageNet16-120')
 algos=('evolution')
 #datasets=('ImageNet16-120')
 studies=(0)
-exps=(2 3 4)
+exps=(5)
 #dgxs=('dgx02' 'dgx04' 'dgx06')
 
 for index in 0 1 2; do
@@ -15,7 +15,7 @@ for index in 0 1 2; do
       for algo in ${algos[@]}; do
         sbatch -c ${NTHREADS} -J "multi_exp${exp}_${algo}_${datasets[${index}]}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG=${algo},DATA=${datasets[${index}]},STUDY=${study},CFG="/dyakovlev/naszilla/naszilla/config_files/${exp}.yaml" bash_scripts/one_experiment.sh
       done
-      sbatch -c ${NTHREADS} -J "multi_exp${exp}_bananas_${datasets[${index}]}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${datasets[${index}]},STUDY=${study},CFG="/dyakovlev/naszilla/naszilla/config_files/${exp}.yaml" bash_scripts/one_experiment_bananas.sh
+#      sbatch -c ${NTHREADS} -J "multi_exp${exp}_bananas_${datasets[${index}]}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${datasets[${index}]},STUDY=${study},CFG="/dyakovlev/naszilla/naszilla/config_files/${exp}.yaml" bash_scripts/one_experiment_bananas.sh
     done
   done
 done
