5b8e55e7d5d4bcd72bfcf3696adfe5ee0f69de43
Daniel yakovlev <daniel.ya.125@gmail.com>
2023-03-22 07:19:31 +0200

    real distance exp

--- bash_scripts/batch_experiments.sh
+++ bash_scripts/batch_experiments.sh
@@ -3,10 +3,10 @@
 TRIALS=9
 NTHREADS=4
 datasets=('cifar10' 'cifar100' 'ImageNet16-120')
-algos=('evolution')
+algos=('local_search')
 #datasets=('ImageNet16-120')
 studies=(0)
-exps=(2)
+exps=(3)
 #dgxs=('dgx02' 'dgx04' 'dgx06')
 
 for index in 0 1 2; do
