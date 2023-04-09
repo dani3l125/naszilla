9e74714c63dcc2408a573e021a05f3115fd38365
Daniel yakovlev <daniel.ya.125@gmail.com>
2023-04-09 12:11:43 +0300

    exp

--- bash_scripts/batch_experiments_bananas.sh
+++ bash_scripts/batch_experiments_bananas.sh
@@ -8,7 +8,7 @@ studies=(0)
 exps=(2 3 4)
 #dgxs=('dgx02' 'dgx04' 'dgx06')
 
-for index in 0 1 2; do
+for index in 0 1; do
   for exp in ${exps[@]}; do
     for study in ${studies[@]}; do
       sbatch -c ${NTHREADS} -J "multi_exp${exp}_bananas_${datasets[${index}]}_study${study}" --export=NTHREADS=${NTHREADS},TRIALS=${TRIALS},ALG='bananas',DATA=${datasets[${index}]},STUDY=${study},CFG="/dyakovlev/naszilla/naszilla/config_files/${exp}.yaml" bash_scripts/one_experiment_bananas.sh
--- bash_scripts/batch_experiments_evolution.sh
+++ bash_scripts/batch_experiments_evolution.sh
@@ -9,7 +9,7 @@ studies=(0)
 exps=(2 3 4)
 #dgxs=('dgx02' 'dgx04' 'dgx06')
 
-for index in 0 1 2; do
+for index in 0; do
   for exp in ${exps[@]}; do
     for study in ${studies[@]}; do
       for algo in ${algos[@]}; do
